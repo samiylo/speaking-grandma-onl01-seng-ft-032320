3aaa99567d43c3fa48e64cc1e694d3f7a50508da
Samiylo Kryshu <skryshu@gmail.com>
2020-03-23 18:50:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,7 +19,7 @@ def speak_to_grandma(phrase)
     return "NO, NOT SINCE 1938!"
   
     
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   
   
