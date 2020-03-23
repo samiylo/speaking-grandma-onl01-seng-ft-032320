e36b6ff58d08b9edc85c413492ec24f4240f092b
Samiylo Kryshu <skryshu@gmail.com>
2020-03-23 18:45:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,9 +17,11 @@ def speak_to_grandma(phrase)
   
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
+  end
     
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  end
   
   else
     return "HU?! SPEAK UP, SONNY"
